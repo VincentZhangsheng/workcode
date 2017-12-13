6d6f82c9bd1d079fdc0dda0923b7968947b82b94
Vincent <515420357@qq.com>
2017-12-13 11:06:03 +0800

    创建路由

---
+++ musicApp_src/components/rank/rank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rank">
+    rank
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Rank',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
